2ab971b935ae0083b54d5bc3ab5b966724c286a5
apsl <dbvaghani@gmail.com>
2022-12-28 18:56:32 +0530

    directory name change for asl_auth

--- chat/lib/chat.dart
+++ chat/lib/chat.dart
@@ -2,11 +2,13 @@ library chat;
 
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+
 part 'firebaseHelper.dart';
 part 'messageObj.dart';
 part 'recentChatObj.dart';

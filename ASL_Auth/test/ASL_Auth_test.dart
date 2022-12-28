db09a7cde3e80af386f0fc5d6915121b96e92f9f
apsl <dbvaghani@gmail.com>
2022-12-28 19:01:50 +0530

    changed name in pubspec.yml

--- ASL_Auth/test/ASL_Auth_test.dart
+++ ASL_Auth/test/ASL_Auth_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ASL_Auth/ASL_Auth.dart';
+import 'package:ASL_Auth/asl_auth.dart';
 
 void main() {
   test('adds one to input values', () {});
